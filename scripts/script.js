06ccad93d1715b2bca34ca44ce035f2e011468c2
Rajveer Singh <singhrajveer4021@gmail.com>
2022-11-27 04:19:04 -0800

    adding a cancel function which hides the save and cancel buttons and also hides the textarea

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,32 @@
 console.log("Beginning Javascript ");
 
 
-const btn = document.querySelector(".theme_button");
+const themeButton = document.querySelector(".theme_button");
+const cancelButton = document.querySelector(".cancel_button");
+const textArea = document.querySelector("textarea");
 
 // Makes the background colors of the sidebar, textarea container, buttons, darker and text should be updated to "Light Theme" // 
 function darkTheme () {
     const aside = document.querySelector("aside");
-    const textArea = document.querySelector("textarea");
     const buttons = document.querySelectorAll("button"); 
     aside.classList.toggle("dark-aside");
     textArea.classList.toggle("dark-textarea");
     for (let item of buttons) {
         item.classList.toggle("dark-buttons");
     }
-    if(btn.textContent === "Dark Theme" ) {
-        btn.textContent = "Light Theme"
+    if(themeButton.textContent === "Dark Theme" ) {
+        themeButton.textContent = "Light Theme"
     }else{
-        btn.textContent = "Dark Theme"
+        themeButton.textContent = "Dark Theme"
     }
-    
 }
 
-btn.addEventListener('click', darkTheme);
+// Hides the save button, cancel button, and textarea //
+function cancel () {
+    const saveCancelButtons = document.querySelector(".button-2")
+    saveCancelButtons.style.visibility = "hidden"
+    textArea.style.visibility = "hidden"
+}
+
+themeButton.addEventListener('click', darkTheme);
+cancelButton.addEventListener('click', cancel)
